5402e405c4e1b293b1aa87f0aa065b8c1c4a8fd1
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2017-07-22 17:07:24 +0200

    init 1
    --

--- .user.js
+++ .user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         QT - AutoTrimps-genBTC
-// @namespace    https://github.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/blob/master/
+// @namespace    https://qtrmnaol.github.io
 // @version      QT - 2.1.5.2-genbtc-12-23-2016+Modular
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC
@@ -12,5 +12,5 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-script';
 //script.src = 'https://genbtc.github.io/AutoTrimps-betadev/AutoTrimps2.js';
-script.src = 'https://raw.githubusercontent.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/master/AutoTrimps2.js';
+script.src = 'https://qtrmnaol.github.io/AutoTrimps2.js';
 document.head.appendChild(script);
