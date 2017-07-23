1faa51b8fc889e449e171b27ad1a2f939502d657
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2017-07-23 08:58:00 +0200

    Fixed UI path
    --

--- NewUI2.js
+++ NewUI2.js
@@ -17,7 +17,7 @@ automationMenuSettingsInit();
 var link1 = document.createElement('link');
 link1.rel = "stylesheet";
 link1.type = "text/css";
-link1.href = 'https://qtrmnaol.github.io/blob/master/tabs.css';
+link1.href = 'https://qtrmnaol.github.io/tabs.css';
 document.head.appendChild(link1);
 
 //Tab make helperfunctions
