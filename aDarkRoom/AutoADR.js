4798cf23897dba499b8da0fac1c120006b48e34a
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2017-07-23 16:19:21 +0200

    refactoring of ATv2
    base for Automation of a dark room

--- aDarkRoom/AutoADR.js
+++ aDarkRoom/AutoADR.js
@@ -1,24 +1,22 @@
 // ==UserScript==
-// @name         QT - AutoTrimpsV2+genBTC
-// @namespace    https://github.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL
-// @version      QT - 2.1.5.2-genbtc-12-23-2016+Modular
-// @description  try to take over the world!
-// @author       zininzinin, spindrjr, belaith, ishakaru, genBTC
-// @include      *trimps.github.io*
-// @include      *kongregate.com/games/GreenSatellite/trimps
+// @name         Auto-aDarkRoom v0.1
+// @namespace    https://github.com/QTrmnaOL/
+// @version      0.1 nonfunctional build
+// @description  Automation of "A Dark Room"
+// @author       QTrmnaOL
+// @include      *http://adarkroom.doublespeakgames.com/*
 // @grant        none
 // ==/UserScript==
-window.alert("script source loaded");
-var ADRVersion = 'QT - 2.1.5.2-genbtc-12-23-2016+Modular';
 
-////////////////////////////////////////////////////////////////////////////////
-//Main Loader Initialize Function (loads first, load everything else)///////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////
-var atscript = document.getElementById('AutoTrimps-script')
-  , base = 'https://raw.githubusercontent.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/master/'
+window.alert("script source loaded");
+var ADRVersion = '0.1 ';
+
+
+var ADRscript = document.getElementById('AutoADR-script')
+  , base = 'https://qtrmnaol.github.io/aDarkRoom/AutoADR.js'
+  // No modules yet.
   , module = 'modules/'
   ;
-if (atscript !== null) {
-    base = atscript.getAttribute('src').replace(/AutoTrimps2\.js$/, '');
+if (ADRscript !== null) {
+    //nothing yet.
 }
--- aDarkRoom/install.user.js
+++ aDarkRoom/install.user.js
@@ -1,20 +1,14 @@
 // ==UserScript==
-// @name         QT - AutoTrimps-genBTC
-// @namespace    https://github.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/blob/master/
-// @version      QT - 2.1.5.2-genbtc-12-23-2016+Modular
-// @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC
-// @include        *trimps.github.io*
-// @include        *kongregate.com/games/GreenSatellite/trimps
+// @name         Auto-aDarkRoom v0.1
+// @namespace    https://github.com/QTrmnaOL/
+// @version      0.1 nonfunctional buiod
+// @description  Automation of "A Dark Room"
+// @author       QTrmnaOL
+// @include      *http://adarkroom.doublespeakgames.com/*
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-script';
-//script.src = 'https://genbtc.github.io/AutoTrimps-betadev/AutoTrimps2.js';
-script.src = 'https://raw.githubusercontent.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/blob/master/AutoTrimps2.js';
+script.id = 'AutoADR-script';
+script.src = 'https://qtrmnaol.github.io/aDarkRoom/AutoADR.js';
 document.head.appendChild(script);
-
-
-// This File is Deprecated, please use .user.js instead (with the leading dot) so TamperMonkey/Greasemonkey can detect it as a script automatically.
-// Other than that they are identical and no change is required on your part if everything is working already.
